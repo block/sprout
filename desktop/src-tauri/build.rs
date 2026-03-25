b4b30cc03d0e86a2707861ce9ca964b3e424b2aa
Wes <wesbillman@users.noreply.github.com>
2026-03-25 15:43:20 -0700

    Set the desktop release relay default (#186)

--- desktop/src-tauri/build.rs
+++ desktop/src-tauri/build.rs
@@ -1,3 +1,14 @@
 fn main() {
+    println!("cargo:rerun-if-env-changed=SPROUT_RELAY_URL");
+    println!("cargo:rerun-if-env-changed=SPROUT_RELAY_HTTP");
+
+    if let Ok(relay_url) = std::env::var("SPROUT_RELAY_URL") {
+        println!("cargo:rustc-env=SPROUT_DESKTOP_BUILD_RELAY_URL={relay_url}");
+    }
+
+    if let Ok(relay_http) = std::env::var("SPROUT_RELAY_HTTP") {
+        println!("cargo:rustc-env=SPROUT_DESKTOP_BUILD_RELAY_HTTP={relay_http}");
+    }
+
     tauri_build::build()
 }
