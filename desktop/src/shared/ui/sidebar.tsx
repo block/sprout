ed0fb27b32c9ca2e5ded3ef7f327edaebfd1dd8a
klopez4212 <klopez4212@gmail.com>
2026-06-05 10:43:35 +0100

    Refine sidebar spacing and inbox dividers

--- desktop/src/shared/ui/sidebar.tsx
+++ desktop/src/shared/ui/sidebar.tsx
@@ -757,7 +757,7 @@ const SidebarMenu = React.forwardRef<
   <ul
     ref={ref}
     data-sidebar="menu"
-    className={cn("flex w-full min-w-0 flex-col gap-0", className)}
+    className={cn("flex w-full min-w-0 flex-col gap-0.5", className)}
     {...props}
   />
 ));
